2515e78537f5103fd98768b2b463e077ba3a70f4
Florian Geiselhart <florian.geiselhart@uni-ulm.de>
2019-03-07 17:15:07 +0100

    ZMQprocessor now even fails better

--- modules/zmq/processors/zmqprocessor.cpp
+++ modules/zmq/processors/zmqprocessor.cpp
@@ -56,21 +56,20 @@ Zmq::Zmq()
                    vec3(0.0001f), InvalidationLevel::Valid, PropertySemantics("Spherical"))
     , cameraLTo_("lookToL", "Look to L", vec3(0.0f), -vec3(100.0f), vec3(100.0f), vec3(0.1f))
     , cameraRTo_("lookToR", "Look to R", vec3(0.0f), -vec3(100.0f), vec3(100.0f), vec3(0.1f))
-	, cameraLUp_("lookUpL", "Look up L", vec3(0.0f, 1.0f, 0.0f), -vec3(100.0f), vec3(100.0f),
-                vec3(0.1f))
+    , cameraLUp_("lookUpL", "Look up L", vec3(0.0f, 1.0f, 0.0f), -vec3(100.0f), vec3(100.0f),
+                 vec3(0.1f))
     , cameraRUp_("lookUpR", "Look up R", vec3(0.0f, 1.0f, 0.0f), -vec3(100.0f), vec3(100.0f),
-                vec3(0.1f)) {
+                 vec3(0.1f)) {
 
     addProperty(camParamsL_);
     camParamsL_.addProperty(cameraLFrom_);
     camParamsL_.addProperty(cameraLTo_);
     camParamsL_.addProperty(cameraLUp_);
 
-	addProperty(camParamsR_);
+    addProperty(camParamsR_);
     camParamsR_.addProperty(cameraRFrom_);
     camParamsR_.addProperty(cameraRTo_);
-	camParamsR_.addProperty(cameraRUp_);
-
+    camParamsR_.addProperty(cameraRUp_);
 
     cameraLFrom_.setReadOnly(true);
     cameraRFrom_.setReadOnly(true);
@@ -82,6 +81,8 @@ Zmq::Zmq()
 
     camera_socket_.setsockopt(ZMQ_IDENTITY, "Inviwo", 6);
     camera_socket_.setsockopt(ZMQ_SUBSCRIBE, "", 0);
+    int linger = 0;
+    camera_socket_.setsockopt(ZMQ_LINGER, &linger, sizeof(linger));
     // int t = 2;
     // camera_socket_.setsockopt(ZMQ_RCVTIMEO, &t, sizeof(t));
     camera_socket_.connect("tcp://localhost:12345");
@@ -92,49 +93,66 @@ Zmq::Zmq()
 Zmq::~Zmq() {
     should_run_ = false;
     thread_.join();
+
+    camera_socket_.close();
+    ctx_.close();
+
+	address_.~message_t();
+	message_.~message_t();
 }
 
 void Zmq::process() {}
 
 void Zmq::receiveZMQ() {
     future_ = dispatchFront([this]() {});
-    while (should_run_ == true) {
+    LogInfo("Receive");
+	while (should_run_ == true) {
         camera_socket_.recv(&address_);
         camera_socket_.recv(&message_);
 
         if (future_.wait_for(std::chrono::milliseconds(0)) == std::future_status::ready) {
-            future_ = dispatchFront([this]() {
-                parseMessage(
-                    json::parse(std::string(static_cast<char*>(message_.data()), message_.size())),
-					std::string(static_cast<char*>(address_.data()), address_.size()));
+            LogInfo("Run outside");
+			future_ = dispatchFront([this]() {
+                if (should_run_ == true) {
+                    LogInfo("Run inside");
+                    parseMessage(json::parse(std::string(static_cast<char*>(message_.data()),
+                                                         message_.size())),
+                                 std::string(static_cast<char*>(address_.data()), address_.size()));
+                }
             });
         }
     }
 }
 
 void Zmq::parseMessage(json content, std::string address) {
-	// Left Eye
-	// Get free fly props from unity
-    vec3 fromL = vec3(content["camVecCamL"]["x"], content["camVecCamL"]["y"], content["camVecCamL"]["z"]);
-	fromL.x = -fromL.x;
-    vec3 toL = vec3(content["camFwdCamL"]["x"], content["camFwdCamL"]["y"], content["camFwdCamL"]["z"]);
+    // Left Eye
+    // Get free fly props from unity
+    vec3 fromL =
+        vec3(content["camVecCamL"]["x"], content["camVecCamL"]["y"], content["camVecCamL"]["z"]);
+    fromL.x = -fromL.x;
+    vec3 toL =
+        vec3(content["camFwdCamL"]["x"], content["camFwdCamL"]["y"], content["camFwdCamL"]["z"]);
     toL.x = -toL.x;
-    vec3 upL = vec3(content["camUpCamL"]["x"], content["camUpCamL"]["y"], content["camUpCamL"]["z"]);
+    vec3 upL =
+        vec3(content["camUpCamL"]["x"], content["camUpCamL"]["y"], content["camUpCamL"]["z"]);
     upL.x = -upL.x;
-	// Set inviwo arcball props
+    // Set inviwo arcball props
     cameraLFrom_.set(fromL);
     cameraLTo_.set(fromL + toL);
     cameraLUp_.set(upL);
 
-	// Right Eye
-	// Get free fly props from unity
-    vec3 fromR = vec3(content["camVecCamR"]["x"], content["camVecCamR"]["y"], content["camVecCamR"]["z"]);
+    // Right Eye
+    // Get free fly props from unity
+    vec3 fromR =
+        vec3(content["camVecCamR"]["x"], content["camVecCamR"]["y"], content["camVecCamR"]["z"]);
     fromR.x = -fromR.x;
-    vec3 toR = vec3(content["camFwdCamR"]["x"], content["camFwdCamR"]["y"], content["camFwdCamR"]["z"]);
+    vec3 toR =
+        vec3(content["camFwdCamR"]["x"], content["camFwdCamR"]["y"], content["camFwdCamR"]["z"]);
     toR.x = -toR.x;
-    vec3 upR = vec3(content["camUpCamR"]["x"], content["camUpCamR"]["y"], content["camUpCamR"]["z"]);
+    vec3 upR =
+        vec3(content["camUpCamR"]["x"], content["camUpCamR"]["y"], content["camUpCamR"]["z"]);
     upR.x = -upR.x;
-	// Set inviwo arcball props
+    // Set inviwo arcball props
     cameraRFrom_.set(fromR);
     cameraRTo_.set(fromR + toR);
     cameraRUp_.set(upR);
