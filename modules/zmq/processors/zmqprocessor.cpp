bf891b874d337a16bd45d8c359d515708f7811ca
Alex Bäuerle <alex.baeuerle@uni-ulm.de>
2019-04-18 18:48:33 +0200

    Mirroring and Adding Implemented
    Mirroring of updates as well as adding Props has now fully been implemented.

--- modules/zmq/processors/zmqprocessor.cpp
+++ modules/zmq/processors/zmqprocessor.cpp
@@ -50,20 +50,9 @@ Zmq::Zmq()
     , type_("property", "Property")
     , name_("name", "Name")
     , address_("address", "Address")
-    , addParamButton_("add", "Add")
-    , camParamsL_("camparamsL", "Camera Parameters L")
-    , camParamsR_("camparamsR", "Camera Parameters R")
-    , cameraLFrom_("lookFromL", "Look From L", vec3(1.0f), -vec3(1000.0f), vec3(1000.0f),
-                   vec3(0.0001f), InvalidationLevel::Valid, PropertySemantics("Spherical"))
-    , cameraRFrom_("lookFromR", "Look From R", vec3(1.0f), -vec3(1000.0f), vec3(1000.0f),
-                   vec3(0.0001f), InvalidationLevel::Valid, PropertySemantics("Spherical"))
-    , cameraLTo_("lookToL", "Look to L", vec3(0.0f), -vec3(100.0f), vec3(100.0f), vec3(0.1f))
-    , cameraRTo_("lookToR", "Look to R", vec3(0.0f), -vec3(100.0f), vec3(100.0f), vec3(0.1f))
-    , cameraLUp_("lookUpL", "Look up L", vec3(0.0f, 1.0f, 0.0f), -vec3(100.0f), vec3(100.0f),
-                 vec3(0.1f))
-    , cameraRUp_("lookUpR", "Look up R", vec3(0.0f, 1.0f, 0.0f), -vec3(100.0f), vec3(100.0f),
-                 vec3(0.1f)) {
+    , addParamButton_("add", "Add") {
 
+    // Allow adding properties to the processor
     addProperty(addParam_);
     type_.addOption("Float", "Float");
     type_.addOption("Int", "Int");
@@ -72,29 +61,26 @@ Zmq::Zmq()
     addParam_.addProperty(type_);
     addParam_.addProperty(name_);
     addParam_.addProperty(address_);
+    addParamButton_.onChange([&]() { addSelectedProperty(); });
     addParam_.addProperty(addParamButton_);
 
-    addProperty(camParamsL_);
-    camParamsL_.addProperty(cameraLFrom_);
-    camParamsL_.addProperty(cameraLTo_);
-    camParamsL_.addProperty(cameraLUp_);
+    // Add the stereo camera right away
+    // Create a new Composite Property with the matching address
+    CompositeProperty* newComp = new CompositeProperty("camera", "Stereo Camera");
+    CompositeProperty* newMirror = new CompositeProperty("camera", "Stereo Camera");
+    // Create the PropertyMapping for later modifications
+    PropMapping pm = PropMapping();
+    pm.address = "camera";
+    pm.type = "Stereo Camera";
+    pm.property = newComp;
+    pm.mirror = newMirror;
+    addStereoCameraProperty(newComp, newMirror);
+    // Add the new Property
+    additionalProps.push_back(pm);
+    addProperty(pm.property);
 
-    addProperty(camParamsR_);
-    camParamsR_.addProperty(cameraRFrom_);
-    camParamsR_.addProperty(cameraRTo_);
-    camParamsR_.addProperty(cameraRUp_);
-
-    cameraLFrom_.setReadOnly(true);
-    cameraRFrom_.setReadOnly(true);
-    cameraRFrom_.setReadOnly(true);
-    cameraLTo_.setReadOnly(true);
-    cameraRTo_.setReadOnly(true);
-    cameraLUp_.setReadOnly(true);
-    cameraRUp_.setReadOnly(true);
-
+    // Start the thread that listens to ZMQ messages
     thread_ = std::thread(&Zmq::receiveZMQ, this);
-
-    addParamButton_.onChange([&]() { addSelectedProperty(); });
 }
 
 Zmq::~Zmq() {
@@ -119,7 +105,8 @@ void Zmq::receiveZMQ() {
         // Address Message:
         zmq::message_t address;
         zmq_socket.recv(&address);
-        std::string address_string = std::string(static_cast<char*>(address.data()), address.size());
+        std::string address_string =
+            std::string(static_cast<char*>(address.data()), address.size());
 
         // Content Message:
         zmq::message_t message;
@@ -127,8 +114,22 @@ void Zmq::receiveZMQ() {
         std::string message_string =
             std::string(static_cast<char*>(message.data()), message.size());
 
-        if (future_.wait_for(std::chrono::milliseconds(0)) == std::future_status::ready) {
-            future_ = dispatchFront( [this, message_string]() { parseMessage(json::parse(message_string)); });
+        try {
+            parseMessage(
+                address_string,
+                json::parse(
+                    message_string));  // Parse all the messages and change the Mirrors accordingly
+        } catch (...) {
+            // Sometimes, the address and message contain incorrect values. This is a HACK to catch
+            // them. It is not a clean solution and should be changed!
+        }
+
+        if (future_.wait_for(std::chrono::milliseconds(0)) ==
+            std::future_status::ready) {  // If the UI is ready
+            future_ =
+                dispatchFront([this, address_string, message_string]() {  // Go to the UI Thread
+                    updateUI();                                           // Update the UI
+                });
         }
     }
 
@@ -136,13 +137,42 @@ void Zmq::receiveZMQ() {
     context.~context_t();
 }
 
-void Zmq::parseMessage(std::string address, json content) {
-    LogWarn(address);
-    LogWarn(content);
-    //if (address)
+void Zmq::updateUI() {
+    for (auto i = additionalProps.begin(); i != additionalProps.end(); ++i) {
+        PropMapping pm = *i;
+        if (pm.type == "Stereo Camera") {
+            // Update Left Eye
+            dynamic_cast<FloatVec3Property*>(pm.property->getPropertyByIdentifier("lookFromL", true))->set(dynamic_cast<FloatVec3Property*>(pm.mirror->getPropertyByIdentifier("lookFromR", true))->get());
+            dynamic_cast<FloatVec3Property*>(pm.property->getPropertyByIdentifier("lookToL", true))->set(dynamic_cast<FloatVec3Property*>(pm.mirror->getPropertyByIdentifier("lookToR", true))->get());
+            dynamic_cast<FloatVec3Property*>(pm.property->getPropertyByIdentifier("lookUpL", true))->set(dynamic_cast<FloatVec3Property*>(pm.mirror->getPropertyByIdentifier("lookUpR", true))->get());
+            // Update Right Eye
+            dynamic_cast<FloatVec3Property*>(pm.property->getPropertyByIdentifier("lookFromR", true))->set(dynamic_cast<FloatVec3Property*>(pm.mirror->getPropertyByIdentifier("lookFromR", true))->get());
+            dynamic_cast<FloatVec3Property*>(pm.property->getPropertyByIdentifier("lookToR", true))->set(dynamic_cast<FloatVec3Property*>(pm.mirror->getPropertyByIdentifier("lookToR", true))->get());
+            dynamic_cast<FloatVec3Property*>(pm.property->getPropertyByIdentifier("lookUpR", true))->set(dynamic_cast<FloatVec3Property*>(pm.mirror->getPropertyByIdentifier("lookUpR", true))->get());
+        } else if (pm.type == "Float") {
+            dynamic_cast<FloatProperty*>(pm.property->getPropertyByIdentifier("value"))->set(dynamic_cast<FloatProperty*>(pm.mirror->getPropertyByIdentifier("value"))->get());
+        } else if (pm.type == "Int") {
+            dynamic_cast<IntProperty*>(pm.property->getPropertyByIdentifier("value"))->set(dynamic_cast<IntProperty*>(pm.mirror->getPropertyByIdentifier("value"))->get());
+        }
+    }
 }
 
-void Zmq::parseStereoCameraMessage(json content) {
+void Zmq::parseMessage(std::string address, json content) {
+    for (auto i = additionalProps.begin(); i != additionalProps.end(); ++i) {
+        PropMapping pm = *i;
+        if (pm.address == address) {
+            if (pm.type == "Stereo Camera") {
+                parseStereoCameraMessage(&pm, content);
+            } else if (pm.type == "Float") {
+                parseFloatMessage(&pm, content);
+            } else if (pm.type == "Int") {
+                parseIntMessage(&pm, content);
+            }
+        }
+    }
+}
+
+void Zmq::parseStereoCameraMessage(PropMapping* prop, json content) {
     // Left Eye
     // Get camera props from unity
     vec3 fromL =
@@ -154,10 +184,14 @@ void Zmq::parseStereoCameraMessage(json content) {
     vec3 upL =
         vec3(content["camUpCamL"]["x"], content["camUpCamL"]["y"], content["camUpCamL"]["z"]);
     upL.x = -upL.x;
-    // Set inviwo arcball props
-    cameraLFrom_.set(fromL);
-    cameraLTo_.set(fromL + toL);
-    cameraLUp_.set(upL);
+
+    // Update Mirror
+    dynamic_cast<FloatVec3Property*>(prop->mirror->getPropertyByIdentifier("lookFromL", true))
+        ->set(fromL);
+    dynamic_cast<FloatVec3Property*>(prop->mirror->getPropertyByIdentifier("lookToL", true))
+        ->set(toL);
+    dynamic_cast<FloatVec3Property*>(prop->mirror->getPropertyByIdentifier("lookUpL", true))
+        ->set(upL);
 
     // Right Eye
     // Get camera props from unity
@@ -170,45 +204,105 @@ void Zmq::parseStereoCameraMessage(json content) {
     vec3 upR =
         vec3(content["camUpCamR"]["x"], content["camUpCamR"]["y"], content["camUpCamR"]["z"]);
     upR.x = -upR.x;
-    // Set inviwo arcball props
-    cameraRFrom_.set(fromR);
-    cameraRTo_.set(fromR + toR);
-    cameraRUp_.set(upR);
+
+    // Update Mirror
+    dynamic_cast<FloatVec3Property*>(prop->mirror->getPropertyByIdentifier("lookFromR", true))
+        ->set(fromL);
+    dynamic_cast<FloatVec3Property*>(prop->mirror->getPropertyByIdentifier("lookToR", true))
+        ->set(toL);
+    dynamic_cast<FloatVec3Property*>(prop->mirror->getPropertyByIdentifier("lookUpR", true))
+        ->set(upL);
+}
+
+void Zmq::parseFloatMessage(PropMapping* prop, json content) {
+    float value = content["value"];
+    dynamic_cast<FloatProperty*>(prop->mirror->getPropertyByIdentifier("value"))->set(value);
+}
+
+void Zmq::parseIntMessage(PropMapping* prop, json content) {
+    int value = content["value"];
+    dynamic_cast<IntProperty*>(prop->mirror->getPropertyByIdentifier("value"))->set(value);
 }
 
 void Zmq::addSelectedProperty() {
     if (name_.get() != "" && address_.get() != "") {
-		// Create a new Composite Property with the matching address
-		CompositeProperty* newComp = new CompositeProperty(name_.get(),name_.get());
-		// Create the PropertyMapping for later modifications
-    	PropMapping pm = PropMapping();
-    	pm.address = address_.get();
-    	pm.type = type_.get();
-    	pm.property = newComp;
+        // Create a new Composite Property with the matching address
+        CompositeProperty* newComp = new CompositeProperty(name_.get(), name_.get());
+        CompositeProperty* newMirror = new CompositeProperty(name_.get(), name_.get());
+        // Create the PropertyMapping for later modifications
+        PropMapping pm = PropMapping();
+        pm.address = address_.get();
+        pm.type = type_.get();
+        pm.property = newComp;
+        pm.mirror = newMirror;
 
-		// Add type-specific props
-		std::string selectedType = type_.getSelectedDisplayName();
-    	if (selectedType == "Float") {
-    	    addFloatProperty(newComp, &pm);
-    	} else if (selectedType == "Int") {
-    	    addIntProperty(newComp, &pm);
-		}
+        // Add type-specific props
+        std::string selectedType = type_.getSelectedDisplayName();
+        if (selectedType == "Float") {
+            addFloatProperty(newComp, newMirror);
+        } else if (selectedType == "Int") {
+            addIntProperty(newComp, newMirror);
+        }
 
-		// Add the new Property
-    	additionalProps.push_back(pm);
-    	addProperty(pm.property);
+        // Add the new Property
+        additionalProps.push_back(pm);
+        addProperty(pm.property);
     } else {
         LogWarn("Please Specify a Name and Adress for your new Property.")
-	}
+    }
 }
 
-void Zmq::addFloatProperty(CompositeProperty* newComp, PropMapping* pm) { 
+void Zmq::addFloatProperty(CompositeProperty* newComp, CompositeProperty* newMirror) {
     newComp->addProperty(new FloatProperty("value", "Value", 0.0, -10000.0, 10000.0));
-    pm->mirror = new FloatProperty("value", "Value", 0.0, -10000.0, 10000.0);
+    newMirror->addProperty(new FloatProperty("value", "Value", 0.0, -10000.0, 10000.0));
 }
 
-void Zmq::addIntProperty(CompositeProperty* newComp, PropMapping* pm) { 
-	newComp->addProperty(new IntProperty("value", "Value", 0, -10000, 10000));
-	pm->mirror = new IntProperty("value", "Value", 0, -10000, 10000);
+void Zmq::addIntProperty(CompositeProperty* newComp, CompositeProperty* newMirror) {
+    newComp->addProperty(new IntProperty("value", "Value", 0, -10000, 10000));
+    newMirror->addProperty(new IntProperty("value", "Value", 0, -10000, 10000));
+}
+
+void Zmq::addStereoCameraProperty(CompositeProperty* newComp, CompositeProperty* newMirror) {
+    // Left Camera
+    CompositeProperty* cameraL = new CompositeProperty("camparamsL", "Camera Parameters L");
+    cameraL->addProperty(new FloatVec3Property(
+        "lookFromL", "Look From L", vec3(1.0f), -vec3(1000.0f), vec3(1000.0f), vec3(0.0001f),
+        InvalidationLevel::Valid, PropertySemantics("Spherical")));
+    cameraL->addProperty(new FloatVec3Property("lookToL", "Look to L", vec3(0.0f), -vec3(100.0f),
+                                               vec3(100.0f), vec3(0.1f)));
+    cameraL->addProperty(new FloatVec3Property("lookUpL", "Look up L", vec3(0.0f, 1.0f, 0.0f),
+                                               -vec3(100.0f), vec3(100.0f), vec3(0.1f)));
+    newComp->addProperty(cameraL);
+    // Left Camera Mirror
+    CompositeProperty* cameraLMirror = new CompositeProperty("camparamsL", "Camera Parameters L");
+    cameraLMirror->addProperty(new FloatVec3Property(
+        "lookFromL", "Look From L", vec3(1.0f), -vec3(1000.0f), vec3(1000.0f), vec3(0.0001f),
+        InvalidationLevel::Valid, PropertySemantics("Spherical")));
+    cameraLMirror->addProperty(new FloatVec3Property("lookToL", "Look to L", vec3(0.0f),
+                                                     -vec3(100.0f), vec3(100.0f), vec3(0.1f)));
+    cameraLMirror->addProperty(new FloatVec3Property("lookUpL", "Look up L", vec3(0.0f, 1.0f, 0.0f),
+                                                     -vec3(100.0f), vec3(100.0f), vec3(0.1f)));
+    newMirror->addProperty(cameraLMirror);
+
+    // Right Camera
+    CompositeProperty* cameraR = new CompositeProperty("camparamsR", "Camera Parameters R");
+    cameraR->addProperty(new FloatVec3Property(
+        "lookFromR", "Look From R", vec3(1.0f), -vec3(1000.0f), vec3(1000.0f), vec3(0.0001f),
+        InvalidationLevel::Valid, PropertySemantics("Spherical")));
+    cameraR->addProperty(new FloatVec3Property("lookToR", "Look to R", vec3(0.0f), -vec3(100.0f),
+                                               vec3(100.0f), vec3(0.1f)));
+    cameraR->addProperty(new FloatVec3Property("lookUpR", "Look up R", vec3(0.0f, 1.0f, 0.0f),
+                                               -vec3(100.0f), vec3(100.0f), vec3(0.1f)));
+    newComp->addProperty(cameraR);
+    // Right Camera Mirror
+    CompositeProperty* cameraRMirror = new CompositeProperty("camparamsR", "Camera Parameters R");
+    cameraRMirror->addProperty(new FloatVec3Property(
+        "lookFromR", "Look From R", vec3(1.0f), -vec3(1000.0f), vec3(1000.0f), vec3(0.0001f),
+        InvalidationLevel::Valid, PropertySemantics("Spherical")));
+    cameraRMirror->addProperty(new FloatVec3Property("lookToR", "Look to R", vec3(0.0f),
+                                                     -vec3(100.0f), vec3(100.0f), vec3(0.1f)));
+    cameraRMirror->addProperty(new FloatVec3Property("lookUpR", "Look up R", vec3(0.0f, 1.0f, 0.0f),
+                                                     -vec3(100.0f), vec3(100.0f), vec3(0.1f)));
+    newMirror->addProperty(cameraRMirror);
 }
 }  // namespace inviwo
